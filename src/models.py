7389fb0a59b72f7cc6b82dbc03c43e07fd98df9d
Surafel <surafelfikru76@gmail.com>
2025-01-30 17:08:44 +0300

    separate different room types

--- src/models.py
+++ src/models.py
@@ -44,29 +44,23 @@ class User(SQLModel, table=True):
     rooms: list["Room"] = Relationship(
         back_populates="participants", link_model=RoomParticipantLink
     )
+    owned_rooms: list["Room"] = Relationship(
+        back_populates="owner",
+    )
     messages: list["Message"] = Relationship(back_populates="owner")
 
-    def _rooms(self) -> list["Room"]:
+    def all_rooms(self) -> list["Room"]:
         """Returns a list of rooms where the user is either a participant or the owner"""
-
-        with Session(engine) as session:
-            room_owned_statement = select(Room).where(Room.owner == self.id)
-            room_participated_statement = (
-                select(Room)
-                .join(RoomParticipantLink)
-                .where(RoomParticipantLink.user_id == self.id)
-            )
-
-            rooms_owned = list(session.exec(room_owned_statement).all())
-            rooms_participated = list(session.exec(room_participated_statement).all())
-            rooms = rooms_owned + rooms_participated
-
-            return rooms
+        return self.rooms + self.owned_rooms
 
 
-class Room(SQLModel, table=True):
-    id: str | None = Field(default_factory=generate_id, primary_key=True)
+class RoomBase(SQLModel):
     room_type: RoomType = Field(default=RoomType.UNIVERSAL)
+
+
+class Room(RoomBase, table=True):
+    id: str | None = Field(default_factory=generate_id, primary_key=True)
+    # room_type: RoomType = Field(default=RoomType.UNIVERSAL)
 
     participants: list[User] = Relationship(
         back_populates="rooms", link_model=RoomParticipantLink
@@ -76,14 +70,22 @@ class Room(SQLModel, table=True):
     )
 
     owner_id: str | None = Field(default=None, foreign_key="user.id")
-    owner: User | None = Relationship(back_populates="rooms")
+    owner: User | None = Relationship(back_populates="owned_rooms")
 
     def add_participant(self, participant: "User"):
         """add a participant to the room and commit to db.
 
         Raises:
             RoomValidationException: if the participant is already in the room
+            RoomValidationException: if the room is private and the room has > 2 participants
         """
+        if self.is_in_room(participant):
+            raise RoomValidationException("Participant is already in the room")
+
+        if self.room_type == RoomType.PRIVATE and len(self.participants) == 2:
+            raise RoomValidationException("Room is private")
+
+        self.participants.append(participant)
 
     def add_message(self, message: "Message"):
         """add a message to the room and commit to db.
@@ -92,11 +94,18 @@ class Room(SQLModel, table=True):
             message (Message): the message to add
 
         Raises:
-            RoomValidationException: if the message is not from the room owner or is not a participant
+            RoomValidationException: if the message is None, not the room owner
+            or not a participant
 
         Returns:
             Message: the message that is added
         """
+        if message.owner and not self.is_in_room(message.owner):
+            raise RoomValidationException("User is not in the room")
+
+        self.messages.append(message)
+
+        return message
 
     def is_in_room(self, user: "User"):
         """checks if a user is in the room as a participant or owner
@@ -107,6 +116,14 @@ class Room(SQLModel, table=True):
         Returns:
             bool: True if the user is in the room
         """
+        return user in self.participants or user.id == self.owner_id
+
+
+class RoomCreate(RoomBase):
+    participants: list[User] = Relationship(
+        back_populates="rooms", link_model=RoomParticipantLink
+    )
+    owner: User | None = Relationship(back_populates="owned_rooms")
 
 
 class Message(SQLModel, table=True):
@@ -114,8 +131,8 @@ class Message(SQLModel, table=True):
     text: str
     created: datetime = Field(default_factory=datetime.now)
 
-    owner_id: str | None = Field(default=None, foreign_key="user.id")
-    owner: User | None = Relationship(back_populates="messages")
+    owner_id: str = Field(default=None, foreign_key="user.id")
+    owner: User = Relationship(back_populates="messages")
 
     # room_id: str | None = Field(default=None, foreign_key="room.id")
     rooms: list[Room] = Relationship(
