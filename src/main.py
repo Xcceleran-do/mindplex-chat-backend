a9d733731ebeb75fcfefdab41ce3e4b3a0bc5887
Surafel <surafelfikru76@gmail.com>
2025-01-30 17:08:22 +0300

    add create_room endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,154 @@
-from typing import Union
+import jwt
+from fastapi import FastAPI, HTTPException, Depends, Body, Header
+from sqlalchemy.orm import Session
+from sqlmodel import Session, select
+from typing import Annotated, List
 
-from fastapi import FastAPI
+from .models import (
+    RoomCreate,
+    RoomType,
+    User,
+    Room,
+    Message,
+    RoomValidationException,
+    engine,
+)
 
 app = FastAPI()
+
+
+JWT_KEY = ""
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+# Dependency to validate JWT and return the user
+def get_user(
+    session: Annotated[Session, Depends(get_session)],
+    authorization: Annotated[str | None, Header()] = None,
+) -> User:
+    if authorization is None or not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid or missing token")
+
+    token = authorization.split(" ")[1]  # Extract the actual token
+
+    try:
+        # Decode and verify the JWT token
+        payload = jwt.decode(token, JWT_KEY, algorithms=["HS256", "RS256"])
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = User(username=payload["username"])
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
+
+
+# Endpoint to create a room (private or universal)
+@app.post("/rooms/", response_model=Room)
+def create_room(
+    room: RoomCreate,
+    user: Annotated[User, Depends(get_user)],
+    session: Annotated[Session, Depends(get_session)],
+):
+    db_room = Room.model_validate(
+        room, context={"owner": session.get(User, room.owner_id)}
+    )
+    session.add(db_room)
+    session.commit()
+    session.refresh(room)
+    return room
+
+
+#
+# # Endpoint to add a participant to a room
+# @app.post("/rooms/{room_id}/participants/")
+# def add_participant_to_room(room_id: str, user_id: str, db: Session = Depends(get_db)):
+#     room = db.query(Room).filter(Room.id == room_id).first()
+#     if room is None:
+#         raise HTTPException(status_code=404, detail="Room not found")
+#
+#     user = get_user_by_id(db, user_id)
+#
+#     try:
+#         room.add_participant(user)
+#         db.commit()
+#     except RoomValidationException as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+#
+#     return {"message": "Participant added successfully"}
+#
+#
+# # Endpoint to send a message to a room
+# @app.post("/rooms/{room_id}/messages/")
+# def send_message(room_id: str, user_id: str, text: str, db: Session = Depends(get_db)):
+#     room = db.query(Room).filter(Room.id == room_id).first()
+#     if room is None:
+#         raise HTTPException(status_code=404, detail="Room not found")
+#
+#     user = get_user_by_id(db, user_id)
+#
+#     if not room.is_in_room(user):
+#         raise HTTPException(status_code=400, detail="User is not in the room")
+#
+#     message = Message(text=text, owner_id=user.id)
+#     try:
+#         room.add_message(message)
+#         db.commit()
+#     except RoomValidationException as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+#
+#     db.refresh(message)
+#     return message
+#
+#
+# # Endpoint to create a private chat (room with only two participants)
+# @app.post("/private_chat/")
+# def create_private_chat(user1_id: str, user2_id: str, db: Session = Depends(get_db)):
+#     user1 = get_user_by_id(db, user1_id)
+#     user2 = get_user_by_id(db, user2_id)
+#
+#     room = Room(room_type=RoomType.PRIVATE)
+#     room.add_participant(user1)
+#     room.add_participant(user2)
+#
+#     db.add(room)
+#     db.commit()
+#     db.refresh(room)
+#
+#     return room
+#
+#
+# # Endpoint to retrieve all rooms a user is part of
+# @app.get("/users/{user_id}/rooms/", response_model=List[Room])
+# def get_rooms_for_user(user_id: str, db: Session = Depends(get_db)):
+#     user = get_user_by_id(db, user_id)
+#     rooms = user.all_rooms()
+#     return rooms
+#
+#
+# # Endpoint to retrieve all messages in a room
+# @app.get("/rooms/{room_id}/messages/", response_model=List[Message])
+# def get_messages_in_room(room_id: str, db: Session = Depends(get_db)):
+#     room = db.query(Room).filter(Room.id == room_id).first()
+#     if room is None:
+#         raise HTTPException(status_code=404, detail="Room not found")
+#
+#     return room.messages
+#
+#
+# # Endpoint to retrieve all users in a room
+# @app.get("/rooms/{room_id}/participants/", response_model=List[User])
+# def get_users_in_room(room_id: str, db: Session = Depends(get_db)):
+#     room = db.query(Room).filter(Room.id == room_id).first()
+#     if room is None:
+#         raise HTTPException(status_code=404, detail="Room not found")
+#
+#     return room.participants
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Engine
-from src.models import User, SQLModel
+from sqlalchemy.inspection import exc
+from src.models import RoomType, RoomValidationException, SQLModel, Room, User, Message
+from fastapi.testclient import TestClient
+from src.main import app
 import pytest
 from sqlmodel import create_engine, Session
 
@@ -9,18 +11,183 @@ def engine_fixture():
     # Create an in-memory SQLite database
     engine = create_engine("sqlite:///:memory:")
     SQLModel.metadata.create_all(engine)
+    yield engine
+    SQLModel.metadata.drop_all(engine)
 
-    return engine
+
+@pytest.fixture(name="session")
+def session_fixture(engine):
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(engine):
+    def get_session_override():
+        with Session(engine) as session:
+            yield session
+
+    app.dependency_overrides[Session] = get_session_override
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture(name="users")
+def users_fixture(session: Session):
+    user = User(username="testuser")
+    user1 = User(username="testuser1")
+    user2 = User(username="testuser2")
+    session.add(user)
+    session.add(user1)
+    session.add(user2)
+    session.commit()
+
+    return [user, user1, user2]
+
+
+@pytest.fixture(name="rooms")
+def rooms_fixture(session: Session, users: list[User]):
+    room = Room(owner=users[0])
+    room1 = Room(room_type=RoomType.PRIVATE, owner=users[1])
+    session.add(room)
+    session.add(room1)
+    session.commit()
+
+    return [room, room1]
+
+
+@pytest.fixture(name="messages")
+def message_fixture(session: Session, users: list[User], rooms: list[Room]):
+    message = Message(text="test message", owner=users[0])
+    message2 = Message(text="Hello world", owner=users[0])
+    message3 = Message(text="Whats up", owner=users[0])
+    message4 = Message(text="", owner=users[1])
+    message5 = Message(text="Bye!!!", owner=users[1])
+    message6 = Message(text="another message", owner=users[2])
+    message7 = Message(text="yet another message", owner=users[2])
+    session.add(message)
+    session.add(message2)
+    session.add(message3)
+    session.add(message4)
+    session.add(message5)
+    session.add(message6)
+    session.add(message7)
+    session.commit()
+
+    return {
+        "0": [message, message2, message3],
+        "1": [message4, message5],
+        "2": [message6, message7],
+    }
 
 
 class TestUser:
 
-    def test_user_creation(self, engine: Engine):
-        # Create a test user
-        user = User(username="testuser")
-        with Session(engine) as session:
-            session.add(user)
-            session.commit()
-            session.refresh(user)
+    def test_all_rooms(self, session: Session, users: list[User], rooms: list[Room]):
 
-        assert user.username == "testuser"
+        # Check owner
+        assert users[0].all_rooms() == [rooms[0]]
+
+        # Add user as participant
+        rooms[0].add_participant(users[1])
+        session.commit()
+
+        assert users[0].all_rooms() == [rooms[0]]
+        assert users[1].all_rooms() == [rooms[0], rooms[1]]
+
+
+class TestRoom:
+    def test_add_participant(
+        self, session: Session, users: list[User], rooms: list[Room]
+    ):
+
+        # Check adding multiple users
+        assert users[0].all_rooms() == [rooms[0]]  # User already exists as owner
+        assert users[1].all_rooms() == [rooms[1]]  # User already exists as owner(room2)
+        assert users[2].all_rooms() == []
+
+        rooms[0].add_participant(users[1])
+        session.commit()
+
+        assert users[0].all_rooms() == [rooms[0]]
+        assert users[1].all_rooms() == [rooms[0], rooms[1]]
+        assert users[2].all_rooms() == []
+
+        rooms[0].add_participant(users[2])
+        session.commit()
+
+        assert users[0].all_rooms() == [rooms[0]]
+        assert users[1].all_rooms() == [rooms[0], rooms[1]]
+        assert users[2].all_rooms() == [rooms[0]]
+
+        # Check adding the same user twice
+        exc_info = None
+        with pytest.raises(RoomValidationException) as e:
+            exc_info = e
+            rooms[0].add_participant(users[0])
+            session.commit()
+        assert exc_info
+        assert exc_info.value
+        assert exc_info.value.args[0] == "Participant is already in the room"
+
+        # Check adding a third user to a private room
+        rooms[1].add_participant(users[0])
+        session.commit()
+        assert rooms[1].participants == [users[0]]
+        exc_info = None
+        with pytest.raises(RoomValidationException) as e:
+            exc_info = e
+            rooms[1].add_participant(users[2])
+            session.commit()
+        assert exc_info
+        assert exc_info.value
+        assert exc_info.value.args[0] == "Room is private"
+
+    def test_add_message(
+        self,
+        session: Session,
+        users: list[User],
+        rooms: list[Room],
+        messages: dict[str, list[Message]],
+    ):
+        assert rooms[0].messages == []
+
+        exc_info = None
+        with pytest.raises(RoomValidationException) as e:
+            exc_info = e
+            rooms[0].add_message(messages.get("1", [])[0])
+            session.commit()
+
+        assert exc_info
+        assert exc_info.value
+        assert exc_info.value.args[0] == "User is not in the room"
+
+        rooms[0].add_participant(users[1])
+        session.commit()
+
+        message = rooms[0].add_message(messages.get("0", [])[0])
+        session.commit()
+
+        assert message
+        assert message.owner == users[0]
+        assert message in rooms[0].messages
+
+    def test_is_in_room(self, session: Session, users: list[User], rooms: list[Room]):
+        assert rooms[0].is_in_room(users[0])
+        assert not rooms[0].is_in_room(users[1])
+        assert not rooms[0].is_in_room(users[2])
+
+        rooms[0].add_participant(users[1])
+        session.commit()
+
+        assert rooms[0].is_in_room(users[0])
+        assert rooms[0].is_in_room(users[1])
+        assert not rooms[0].is_in_room(users[2])
+
+        rooms[0].add_participant(users[2])
+        session.commit()
+
+        assert rooms[0].is_in_room(users[0])
+        assert rooms[0].is_in_room(users[1])
+        assert rooms[0].is_in_room(users[2])
