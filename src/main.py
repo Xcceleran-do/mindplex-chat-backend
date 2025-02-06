13b2949fe4bebd689538437f9a4724fa358e8992
Surafel <surafelfikru76@gmail.com>
2025-02-06 10:03:01 +0300

    add room and room messages endpoints

--- src/main.py
+++ src/main.py
@@ -95,88 +95,22 @@ def create_room(
     return db_room
 
 
-#
-# # Endpoint to add a participant to a room
-# @app.post("/rooms/{room_id}/participants/")
-# def add_participant_to_room(room_id: str, user_id: str, db: Session = Depends(get_db)):
-#     room = db.query(Room).filter(Room.id == room_id).first()
-#     if room is None:
-#         raise HTTPException(status_code=404, detail="Room not found")
-#
-#     user = get_user_by_id(db, user_id)
-#
-#     try:
-#         room.add_participant(user)
-#         db.commit()
-#     except RoomValidationException as e:
-#         raise HTTPException(status_code=400, detail=str(e))
-#
-#     return {"message": "Participant added successfully"}
-#
-#
-# # Endpoint to send a message to a room
-# @app.post("/rooms/{room_id}/messages/")
-# def send_message(room_id: str, user_id: str, text: str, db: Session = Depends(get_db)):
-#     room = db.query(Room).filter(Room.id == room_id).first()
-#     if room is None:
-#         raise HTTPException(status_code=404, detail="Room not found")
-#
-#     user = get_user_by_id(db, user_id)
-#
-#     if not room.is_in_room(user):
-#         raise HTTPException(status_code=400, detail="User is not in the room")
-#
-#     message = Message(text=text, owner_id=user.id)
-#     try:
-#         room.add_message(message)
-#         db.commit()
-#     except RoomValidationException as e:
-#         raise HTTPException(status_code=400, detail=str(e))
-#
-#     db.refresh(message)
-#     return message
-#
-#
-# # Endpoint to create a private chat (room with only two participants)
-# @app.post("/private_chat/")
-# def create_private_chat(user1_id: str, user2_id: str, db: Session = Depends(get_db)):
-#     user1 = get_user_by_id(db, user1_id)
-#     user2 = get_user_by_id(db, user2_id)
-#
-#     room = Room(room_type=RoomType.PRIVATE)
-#     room.add_participant(user1)
-#     room.add_participant(user2)
-#
-#     db.add(room)
-#     db.commit()
-#     db.refresh(room)
-#
-#     return room
-#
-#
-# # Endpoint to retrieve all rooms a user is part of
-# @app.get("/users/{user_id}/rooms/", response_model=List[Room])
-# def get_rooms_for_user(user_id: str, db: Session = Depends(get_db)):
-#     user = get_user_by_id(db, user_id)
-#     rooms = user.all_rooms()
-#     return rooms
-#
-#
-# # Endpoint to retrieve all messages in a room
-# @app.get("/rooms/{room_id}/messages/", response_model=List[Message])
-# def get_messages_in_room(room_id: str, db: Session = Depends(get_db)):
-#     room = db.query(Room).filter(Room.id == room_id).first()
-#     if room is None:
-#         raise HTTPException(status_code=404, detail="Room not found")
-#
-#     return room.messages
-#
-#
-# # Endpoint to retrieve all users in a room
-# @app.get("/rooms/{room_id}/participants/", response_model=List[User])
-# def get_users_in_room(room_id: str, db: Session = Depends(get_db)):
-#     room = db.query(Room).filter(Room.id == room_id).first()
-#     if room is None:
-#         raise HTTPException(status_code=404, detail="Room not found")
-#
-#     return room.participants
+@app.get("/rooms/{room_id}", response_model=Room)
+async def get_room(
+    room_id: str,
+    session: Annotated[Session, Depends(get_session)],
+    user: Annotated[User, Depends(get_user_dep)],
+):
+    room = await Room.get_by_id(room_id, session, raise_exc=True)
+    return room
+
+
+@app.get("/rooms/{room_id}/message", response_model=list[Message])
+async def get_room_messages(
+    room_id: str,
+    session: Annotated[Session, Depends(get_session)],
+    user: Annotated[User, Depends(get_user_dep)],
+):
+    room = await Room.get_by_id(room_id, session, raise_exc=True)
+    assert room is not None
+    return room.messages
