4bd4eff76e825a42dc2eec46df910b79c229128a
surafel_fikru <surafelfikru76@gmail.com>
2025-04-14 16:04:13 +0300

    add room route parameter

---
+++ test_ui/src/routes/[user]/[room]/+page.server.ts
@@ -0,0 +1,29 @@
+import { error, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { testUsers } from '$lib/';
+import { getRooms } from '$lib/api';
+
+export const load: PageServerLoad = async ({ params, parent }) => {
+	// Get query parameter
+	
+	let parentData = await parent()
+
+	const username = params.user;
+	const remoteUser = params.room;
+
+	if (parentData.user == null) {
+		// redirect to home page
+		throw redirect(302, '/');
+	}
+
+	let currentUserRooms = await getRooms(parentData.user.token, parentData.user.username)
+
+	console.log("currentUserRooms: ", currentUserRooms)
+
+	return {
+		remoteUser,
+		currentUserRooms
+	}
+
+};
+
---
+++ test_ui/src/routes/[user]/[room]/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	// get test users from server
+
+	import type { PageProps } from './$types';
+	import { getRooms, sendMessage, createSocket } from '$lib/api';
+
+	let { data }: PageProps = $props();
+
+	let user = $state(data.user);
+	//let room = $state(data.remoteUser);
+	//let room;
+	//if (data.remoteUser) {
+	//
+	//}
+
+	let messages: HTMLDivElement
+	let inp: HTMLInputElement
+
+</script>
+
+<div>
+	<h2>WebSocket Chat Test</h2>
+	<h2>Hey {user?.username} you are chatting with</h2>
+	<div id="messages" bind:this={messages}></div>
+	<input type="text" id="messageInput" placeholder="Type a message..." bind:this={inp}>
+	<button id="sendButton" onclick={sendMessage}>Send</button>
+	<button onclick={createSocket}>Connect</button>
+</div>
+
+<style>
+	#messages { border: 1px solid #ccc; padding: 10px; height: 200px; overflow-y: scroll; }
+</style>
