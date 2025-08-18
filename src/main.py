b2a7b5315ee1ba73ef9644df909bbb5b393a7517
surafel_fikru <surafelfikru76@gmail.com>
2025-08-18 16:49:16 +0300

    add axiom logging

--- src/main.py
+++ src/main.py
@@ -9,22 +9,33 @@ from .chat import socket, sse
 from .models import SQLModel, engine, wait_for_postgres
 from .tasks import remove_expired_rooms_once
 from .routers import users, rooms
-
+from axiom_py.logging import AxiomHandler
+import axiom_py
+import logging
 
 dotenv.load_dotenv()
 
+def setup_axiom_logger():
+    client = axiom_py.Client(os.getenv("AXIOM_TOKEN"))
+    handler = AxiomHandler(client, os.getenv("AXIOM_DATASET", "mindplex-chat-app"))
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+    # start axiom
+    setup_axiom_logger()
+
     # TODO: create proper db migrations. see https://alembic.sqlalchemy.org/en/latest/ 
     SQLModel.metadata.create_all(engine)
     wait_for_postgres()
     yield
     # SQLModel.metadata.drop_all(engine)
 
-
 app = FastAPI(lifespan=lifespan)
 
+logging.getLogger(__name__).info("Axiom setup complelte!!!")
 
 # routers
 app.include_router(users.router)
